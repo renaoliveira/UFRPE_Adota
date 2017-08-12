2f8ea40f93a6a390aa2ef14fd1a9a3a46689ad31
lidymonteirowm <lidianedesouzamonteiro@gmail.com>
2017-08-12 13:40:37 -0300

    Repositório Produto  - Lista

---
+++ src/repositorio/RepositorioProdutoLista.java
@@ -0,0 +1,123 @@
+package repositorio;
+
+import excecao.ProdutoCadastradoException;
+import excecao.ProdutoInexistenteException;
+import interfaces.IRepositorioProduto;
+import negocio.Produto;
+
+public class RepositorioProdutoLista implements IRepositorioProduto{
+	private Produto produto;
+	private RepositorioProdutoLista prox;
+	public static int tam; /* Tamanho da lista, atributo pertence a classe.*/
+	private int indice; /* Respectiva posição da lista, atributo pertence ao objeto */
+	
+	public RepositorioProdutoLista(){
+		
+	}
+	
+	public int getIndice(){
+		return indice;
+	}
+	
+	public RepositorioProdutoLista getProx(){
+		return prox;
+	}
+	
+	
+	/* Pega uma célula específico da lista. Recebe como paramêtro id do produto
+	 * e procura um nó com o respectivo idProduto na lista. Retorna o nó que contém o idProduto
+	 * ou retorna null; 
+	*/
+	public RepositorioProdutoLista getNo(String idProduto){
+		RepositorioProdutoLista atual = prox;
+		
+		while(idProduto != produto.getIdProduto()){
+			if(atual == null){
+				return null;
+			} else {
+				return atual.getNo(idProduto);
+			}
+		}
+		return atual;
+	}
+	
+	
+	/* Insere um produto no RepositórioProdutoLista.
+	 * Recebe como paramêtro o produto a ser inserido. Verifica se o produto já foi cadastrado na lista. 
+	 * Se não existir, insere o produto, aumenta o tamanho da lista, instancia um novo objeto prox e armazena sua posição na lista. 
+	 * Se o produto já estiver cadastrado na lista, insere na próxima posição nula. 
+	*/
+	public void inserir(Produto produto) throws ProdutoCadastradoException{
+		if(this.produto == null){
+			this.produto = produto;
+			tam++; /* Aumenta o tamanho da lista*/
+			prox = new RepositorioProdutoLista();
+			indice = tam; /* Quando o objeto é instanciado, armazena sua posição na lista */
+		} else {
+			prox.inserir(produto);
+		}
+	} 
+
+	
+	/* Procura um produto no RepositórioProdutoLista e retorna o produto procurado. 
+	 * Recebe como o parâmetro o id do produto e enquanto não encontrar o produto com o referido id, 
+	 * faz a busca na próxima célula. 
+	 * 
+	*/
+	public Produto procurar(String idProduto) throws ProdutoInexistenteException {
+		boolean achou = false;
+		String n;
+		while(!achou && produto!=null){
+			n = produto.getIdProduto();
+			if(n.equals(idProduto)){
+				achou = true;
+			} else {
+				return prox.procurar(idProduto);
+			}
+		}
+		if(produto == null){
+			ProdutoInexistenteException e = new ProdutoInexistenteException(idProduto);
+			throw e;
+		}
+		return produto;
+	}
+	
+	/* Remove um elemento do RepositórioProdutoLista. 
+	 * Recebe como parâmetro o id do produto que será excluído do repositório e faz a remoção. 
+	*/
+	public void remover(String idProduto) { 
+		if(produto != null){
+			if(idProduto == produto.getIdProduto()){
+				produto = getProx().produto;
+				prox = getProx().prox;
+			}
+			else{
+				getProx().remover(idProduto);
+			}
+		}
+	}
+	
+	public void atualizar(Produto produto) {
+		produto = getNo(produto.getIdProduto()).produto;
+	}
+	
+	/* Verifica se o produto existe no RepositorioProdutoLista. 
+	 * Recebe como parâmetro o id do produto, retorna true se existir e false caso não existir. 
+	*/
+	public boolean existe(String idProduto){
+		boolean achou = false;
+		String n;
+		while (!achou && produto != null) {
+			n = produto.getIdProduto();
+			if (n.equals(idProduto)) {
+				achou = true;
+			} else {
+				return prox.existe(idProduto);
+			}
+		}
+		if(produto == null)
+			return false;
+		else return true;
+	}
+}
+
